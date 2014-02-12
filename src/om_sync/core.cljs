1d34d8d90cda9663f222e35c614b613ed40f88bd
David Nolen <david.nolen@gmail.com>
2014-02-12 00:55:31 -0500

    wip

--- src/om_sync/core.cljs
+++ src/om_sync/core.cljs
@@ -1,22 +1,42 @@
 (ns om-sync.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljs.core.async :as async :refer [>! <!]]
+  (:require [cljs.core.async :as async :refer [put! chan alt!]]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [om-sync.util :refer [edn-xhr]]))
 
 (defn om-sync
   ([data owner] (om-sync data owner nil))
-  ([{:keys [url coll]} owner {:keys [view tx-chan]}]
+  ([{:keys [url coll]} owner {:keys [view hdlrs] :as opts}]
+    (assert (not (nil? url)) "om-sync component not given url")
     (reify
       om/IInitState
       (init-state [_]
         {:kill-chan (chan)})
       om/IWillMount
       (will-mount [_]
-        (go (loop []
-              ))
-        (go (loop []
-              )))
+        (let [kill-chan (om/get-state owner :kill-chan)
+              tx-chan   (om/get-shared owner :tx-chan)]
+          (go (loop []
+                (let [[v c] (alt! [kill-chan tx-chan])]
+                  (if (= c kill-chan)
+                    :done
+                    (do
+                      (if-let [hdlr (get hdlrs (first v))]
+                        (apply hdlr v))
+                      (recur))))))))
+      om/IWillUnmount
+      (will-unmount [_]
+        (let [kill-chan (om/get-state owner :kill-chan)]
+          (put! kill-chan (js/Date.))))
       om/IRender
       (render [_]
-        (om/build (:view opts) coll opts)))))
+        (om/build view coll opts)))))
+
+(comment
+  (om/build om-sync (:classes app)
+    {:opts {:view classes-view
+            :hdlrs {:class/edit-title 1
+                    :class/create 2
+                    :on-error 3}}})
+  )
--- src/om_sync/util.clj
+++ src/om_sync/util.clj
@@ -1,1 +1,21 @@
-(ns om-sync.util)
+(ns om-sync.util
+  (:require [cljs.reader :as reader]
+            [goog.events :as events])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+      (send url (meths method) (when data (pr-str data))
+        #js {"Content-Type" "application/edn"}))))
